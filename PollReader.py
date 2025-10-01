ff60211abd5818e37d8cef1a59fac36bcb1b3f28
Olivia Lombardo <livlomb@umich.edu>
2025-10-01 17:40:40 -0400

    Finsihed def likely_voter_polling_average

--- PollReader.py
+++ PollReader.py
@@ -83,7 +83,7 @@ class PollReader():
         """
         max_harris = max(self.data_dict['Harris result'])
         max_trump = max(self.data_dict['Trump result'])
-        
+
         if max_harris > max_trump:
             return f"Harris with {(max_harris *100):.1f}"
         elif max_trump > max_harris:
@@ -100,7 +100,20 @@ class PollReader():
             tuple: A tuple containing the average polling percentages for Harris and Trump
                    among likely voters, in that order.
         """
-        pass
+        harris_sum = 0
+        trump_sum = 0
+        counter = 0
+
+        for index in range(len(self.data_dict['sample type'])):
+            if self.data_dict['sample type'][index] == "LV":
+                harris_sum += self.data_dict['Harris result'][index]
+                trump_sum += self.data_dict['Trump result'][index]
+                counter += 1
+        
+        if counter > 0:
+            return (harris_sum/counter, trump_sum/counter)
+        else:
+            return (0.0,0.0)
 
 
     def polling_history_change(self):
